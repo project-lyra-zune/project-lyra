dfa26a99ef318730c17f3682402e186cd2a8848b
magicisinthehole <592339+magicisinthehole@users.noreply.github.com>
2026-07-27 13:47:42 -0700

    Pin the browser bootstrap to the published 1.3.1 package
    publish-repo.sh stamps the digest of the platform .zmod it serves. lyraboot has
    to be rebuilt whenever this moves, or it rejects what it downloads.

--- src/lyraboot/boot_pin.h
+++ src/lyraboot/boot_pin.h
@@ -6,6 +6,6 @@
 
 #define LYRA_PIN_VERSION  "1.3.1"
 #define LYRA_PIN_URL      "http://install.zune.moe/lyra-1.3.1.zmod"
-#define LYRA_PIN_SHA256   "5a130936f7ab40d0be540f82dba82a621bfc6776983b824ccf6045e062fc1013"
+#define LYRA_PIN_SHA256   "9c6e1ac7d8ff86d28b33904435fefe38fe96e2427e4b0d384886c4f7c491c3ee"
 
 #endif /* BOOT_PIN_H */
